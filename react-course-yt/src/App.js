478b187214463b98bad7c07e1e7734bbbe7af7dd
Michelle <michelle-jolink@hotmail.com>
2022-09-24 09:18:47 +0200

    render all movies

--- react-course-yt/src/App.js
+++ react-course-yt/src/App.js
@@ -56,7 +56,9 @@ const App = () => {
                 movies?.length > 0 
                 ? (
                 <div className="container">
-                    <MovieCard movie1={movies[0]}/>
+                    {movies.map((movie) =>
+                    <MovieCard movie={movie}/>
+                    )}
                 </div>
                 )   :
                 (
