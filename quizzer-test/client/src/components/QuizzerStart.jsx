7213ac1f1caacec3ab07ed1102a66569c98661de
Michelle <michelle-jolink@hotmail.com>
2022-12-04 14:48:07 +0100

    setup

--- quizzer-test/client/src/components/QuizzerStart.jsx
+++ quizzer-test/client/src/components/QuizzerStart.jsx
@@ -6,6 +6,7 @@ export default function StartQuizzer() {
         <div className="room-container">
             <button className="btn-submit">
                 START THE QUIZ
+                name:
             </button>
         </div>
     )
