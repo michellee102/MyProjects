24691c5bdb606c25c11e7e94c2770d9e74fb777b
Michelle <michelle-jolink@hotmail.com>
2022-12-25 14:41:09 +0100

    Lesson 4: Define controllers without body and add to user route

--- technotes_project/controllers/usersController.js
+++ technotes_project/controllers/usersController.js
@@ -1,5 +1,40 @@
 const User = require('../models/User')
 const Note = require('../models/Note')
+// For handling exceptions inside async routes and passing them to your express error handler
+const asyncHandler = require('express-async-handler')
+// For hashing user password before saving it
+const bcrypt = require('bcrypt')
+
+// @desc: Get all users
+// @route: GET /users
+// @access: Private
+const getAllUsers = asyncHandler(async (req, res) => {
+
+})
+
+// @desc: Create new user
+// @route: POST /users
+// @access: Private
+const createNewUser = asyncHandler(async (req, res) => {
+
+})
+
+// @desc: Update a user
+// @route: PATCH /users
+// @access: Private
+const updateUser = asyncHandler(async (req, res) => {
+
+})
+
+// @desc: Delete a user
+// @route: DELETE /users
+// @access: Private
+const deleteUser = asyncHandler(async (req, res) => {
+
+})
 
 
 
+module.exports = {
+  createNewUser, getAllUsers, updateUser, deleteUser
+}
