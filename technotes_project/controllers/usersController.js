e77254132d102e48f57431e981a570f20809f607
Michelle <michelle-jolink@hotmail.com>
2022-12-25 14:52:15 +0100

    Lesson 4: Implement body of get all users controller

--- technotes_project/controllers/usersController.js
+++ technotes_project/controllers/usersController.js
@@ -9,7 +9,13 @@ const bcrypt = require('bcrypt')
 // @route: GET /users
 // @access: Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+  // .select(): which fields you want or do not want to receive
+  // .lean(): With this mongoose only returns the data, not methods of this model like save, we're not changing data in this route so we don't need it
+  const users = await User.find().select('-password')
+  if (!users) {
+    return res.status(400).json({ message: 'Sorry, no users found.' })
+  }
+  res.json(users)
 })
 
 // @desc: Create new user
